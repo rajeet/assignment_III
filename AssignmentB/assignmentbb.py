63b9845a2e350130307bdd80e0eaf1654096f92b
Rajeet <mangratirajit@gmail.com>
2020-07-12 00:45:28 +0545

    all done

--- AssignmentB/assignmentbb.py
+++ AssignmentB/assignmentbb.py
@@ -1,6 +1,10 @@
 import csv
+import os
 import sys
 from tabulate import tabulate
+from tempfile import NamedTemporaryFile
+import shutil
+
 
 class Academy:
     def course_of_content(self):
@@ -42,7 +46,14 @@ class Academy:
         with open("studentdata.csv", 'a+', newline='') as student_file:
             fieldnames = ['Student_Name', 'Age', 'Contact', 'Deposit', 'Course']
             writer = csv.DictWriter(student_file, fieldnames=fieldnames)
-            writer.writerow({'Student_Name': name, 'Age': age, 'Contact': contact, 'Deposit': deposite, 'Course': course})
+            # edit here 
+            if os.path.getsize("studentdata.csv") > 0:
+                print("not empty")
+                writer.writerow({'Student_Name': name, 'Age': age, 'Contact': contact, 'Deposit': deposite, 'Course': course})     
+            else:
+                print("empty")
+                writer.writeheader()
+                writer.writerow({'Student_Name': name, 'Age': age, 'Contact': contact, 'Deposit': deposite, 'Course': course})
 
 
         print("Thank you for registering")
@@ -55,24 +66,92 @@ class Academy:
         table = []
 
         # edit ends here 
+        # if os.path.isfile("studentdata.csv"):
+        #     with open('studentdata.csv', mode='r') as read_student_data:
+        #         csv_reader = csv.DictReader(read_student_data)
+        #         line_count = 0
+        #         for row in csv_reader:
+        #             if line_count == 0:
+        #                 table.append(list(row))
+        #                 line_count += 1
+        #             dat = [row['Student_Name'], row['Age'], row['Contact'], row['Deposit'], row['Course']]
+        #             table.append(dat)
+        #     line_count += 1
+        #     print(tabulate(table))
+            
+        # else:
+        #     print("Fild doesnot exists")
 
-        with open('studentdata.csv', mode='r') as read_student_data:
-             csv_reader = csv.DictReader(read_student_data)
-             line_count = 0
-             for row in csv_reader:
-                if line_count == 0:
-                    table.append(list(row))
-                    line_count += 1
-                dat = [row['Student_Name'], row['Age'], row['Contact'], row['Deposit'], row['Course']]
-                table.append(dat)
-        line_count += 1
-        print(tabulate(table))
+        # self.ifelse()
+
+        try:
+            with open('studentdata.csv', mode='r') as read_student_data:
+                csv_reader = csv.DictReader(read_student_data)
+                line_count = 0
+                for row in csv_reader:
+                    if line_count == 0:
+                        table.append(list(row))
+                        line_count += 1
+                    dat = [row['Student_Name'], row['Age'], row['Contact'], row['Deposit'], row['Course']]
+                    table.append(dat)
+            line_count += 1
+            print(tabulate(table))
+        except (FileNotFoundError, IOError):
+            print("File doesnot exists")
+        
         self.ifelse()
 
     def update_students(self, contact):
         print(contact)
 
+        data = dict()
+        fake = []
+        input_file = csv.DictReader(open("studentdata.csv"))
+        for row in input_file:
+            fake.append(row)
 
+        for i in fake:
+            if int(i["Contact"]) == contact:
+                print(f"Name: {i['Student_Name']}")
+                print(f"Age: {i['Age']}")
+                print(f"Contact: {i['Contact']}")
+                print(f"Deposit: {i['Deposit']}")
+                print(f"Course: {i['Course']}")
+
+                fake.remove(i)
+                print("_"*50)
+                name = input("Update your Name: ")
+                age = input("Update your age: ")
+                contact= int(input("Update your contact number: "))
+                deposit = int(input("Update S.N. for Deposit \n1: For full \n2: In installment\n>"))
+                if deposit == 1: 
+                    deposite = 20000 
+                else:
+                    deposite = 10000
+                course = input("HTML Basics \nCSS Basics \nJS Basics\nPHP Basics \npython Basics\n")
+                print(f"{name} {age} {contact} {deposite} {course}")
+                print("_"*50)
+
+                fake.append({'Student_Name': name, 'Age': age, 'Contact': contact, 'Deposit': deposite, 'Course': course})
+                break
+            else: 
+                print("False")
+
+    # edits start here
+        with open("newstudentdata.csv", 'a+', newline='') as student_file:
+            fieldnames = ['Student_Name', 'Age', 'Contact', 'Deposit', 'Course']
+            writer = csv.DictWriter(student_file, fieldnames=fieldnames)
+            # edit here 
+            writer.writeheader()
+            for i in fake:
+                    writer.writerow({'Student_Name': i['Student_Name'], 'Age': i['Age'], 'Contact': i["Contact"], 'Deposit': i['Deposit'], 'Course': i['Course']})     
+
+
+        print("Thank you for Updating")
+        old = "studentdata.csv"
+        new = "newstudentdata.csv"
+        os.remove("studentdata.csv")
+        os.rename(new, old)
 
         self.ifelse()
 
@@ -80,11 +159,30 @@ class Academy:
 
 
     def delete_students(self, contact):
+        deletedata = []
         print(contact)
-        with open("studentdata.csv") as csvfile:
-            reader = csv.DictReader(csvfile)
+        input_file = csv.DictReader(open("studentdata.csv"))
+        for row in input_file:
+            deletedata.append(row)
+        
+        for i in deletedata:
+            print (i)
+            if int(i["Contact"]) == contact:
+                deletedata.remove(i)
+        print(deletedata)
+        with open("newstudentdata.csv", 'w', newline='') as student_file:
+            fieldnames = ['Student_Name', 'Age', 'Contact', 'Deposit', 'Course']
+            writer = csv.DictWriter(student_file, fieldnames=fieldnames)
+            # edit here 
+            writer.writeheader()
+            for i in deletedata:
+                    writer.writerow({'Student_Name': i['Student_Name'], 'Age': i['Age'], 'Contact': i["Contact"], 'Deposit': i['Deposit'], 'Course': i['Course']})     
             
-                        
+        print("Data Deleted")
+        old = "studentdata.csv"
+        new = "newstudentdata.csv"
+        os.remove("studentdata.csv")
+        os.rename(new, old)        
 
         
         self.ifelse()
